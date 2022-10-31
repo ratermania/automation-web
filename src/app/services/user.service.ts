a01e672ad74a73a37d05a194e7bb1d202781ac6f
Tom Raterman <tom@tomraterman.com>
2022-10-30 22:18:16 -0400

    Minor tweaks

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,60 +8,59 @@ import { PasswordChange } from "../models/password-change.model";
 import { User } from "../models/user.model";
 
 @Injectable({
-	providedIn: 'root'
+    providedIn: 'root'
 })
 export class UserService {
-	private _user: BehaviorSubject<User> = new BehaviorSubject<User>(null);
+    private _user: BehaviorSubject<User> = new BehaviorSubject<User>(null);
 
-	constructor(private _http: HttpClient) { }
+    constructor(private _http: HttpClient) { }
 
-	getUsers(): Observable<User[]> {
-		return this._http.get<User[]>(`${environment.identityUrl}/users/`);
-	}
+    getUsers(): Observable<User[]> {
+        return this._http.get<User[]>(`${environment.identityUrl}/users/`);
+    }
 
-	getUserFromToken(): BehaviorSubject<User> {
-		if (!this._user.value?.username) {
-			const token: string = sessionStorage.getItem('token');
-			this.setUserFromToken(token);
-		}
+    getUserFromToken(): BehaviorSubject<User> {
+        if (!this._user.value?.username) {
+            const token: string = sessionStorage.getItem('token');
+            this.setUserFromToken(token);
+        }
 
-		return this._user;
-	}
+        return this._user;
+    }
 
-	login(userLogin: Login): Observable<any> {
-		return this._http.post(`${environment.identityUrl}/users/login`, userLogin).pipe(
-			map((data: any) => {
-				sessionStorage.setItem('token', data.token);
+    login(userLogin: Login): Observable<any> {
+        return this._http.post(`${environment.identityUrl}/users/login`, userLogin).pipe(
+            map((data: any) => {
+                sessionStorage.setItem('token', data.token);
+                this.setUserFromToken(data.token);
+            })
+        );
+    }
 
-				this.setUserFromToken(data.token);
-			})
-		);
-	}
+    logout() {
+        sessionStorage.clear();
+        this._user.next(new User());
+    }
 
-	logout() {
-		sessionStorage.clear();
-		this._user.next(new User());
-	}
+    changePassword(passwordChange: PasswordChange): Observable<any> {
+        return this._http.put(`${environment.identityUrl}/users/password`, passwordChange);
+    }
 
-	changePassword(passwordChange: PasswordChange): Observable<any> {
-		return this._http.put(`${environment.identityUrl}/users/password`, passwordChange);
-	}
+    update(user: User) {
+        return this._http.put(`${environment.identityUrl}/users`, user);
+    }
 
-	update(user: User) {
-		return this._http.put(`${environment.identityUrl}/users`, user);
-	}
+    private setUserFromToken(token: string) {
+        if (!token) {
+            return;
+        }
 
-	private setUserFromToken(token: string) {
-		if (!token) {
-			return;
-		}
+        const base64User: string = token.split('.')[1]
+            .replace('-', '+')
+            .replace('_', '/');
 
-		const base64User: string = token.split('.')[1]
-			.replace('-', '+')
-			.replace('_', '/');
+        const user: User = JSON.parse(atob(base64User));
 
-		const user: User = JSON.parse(atob(base64User));
-
-		this._user.next(user);
-	}
+        this._user.next(user);
+    }
 }
