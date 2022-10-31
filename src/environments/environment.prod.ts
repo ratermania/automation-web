d0c018741a9c9fd5be457855437059b97aa3a19a
Tom Raterman <tom@tomraterman.com>
2022-10-31 08:18:16 -0400

    Production environment settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+    production: true,
+    apiUrl: "https://automation.partsourceportal.net",
+    identityUrl: "https://api.partsourceportal.net",
+    applicationTitle: "Partsource Automation Portal"
 };
